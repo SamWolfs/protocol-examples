f52c7474dcc58dcaef86aad9c7e3f00f284e5f2b
Sam Wolfs <be.samwolfs@gmail.com>
2022-05-15 21:41:59 +0200

    Format + allow CORS

--- websocket/servers/gorilla_server/main.go
+++ websocket/servers/gorilla_server/main.go
@@ -5,14 +5,18 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
 	pb "github.com/SamWolfs/protocol-examples/websocket/servers/gorilla_server/proto"
+	"github.com/gorilla/websocket"
 	"google.golang.org/protobuf/proto"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
-var upgrader = websocket.Upgrader{} // use default options
+var upgrader = websocket.Upgrader{
+	// Allow cross origin requests from browser
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	}}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -32,11 +36,12 @@ func echo(w http.ResponseWriter, r *http.Request) {
 			log.Fatalln("Failed to decode command:", err)
 		}
 		switch command.Payload.(type) {
-			case *pb.Command_Echo:
-				log.Println("Echo Command")
-			case *pb.Command_Increment:
-				command.GetIncrement().Value++
-				log.Println("Increment Command")
+		case *pb.Command_Echo:
+			log.Println("Echo Command")
+		case *pb.Command_Increment:
+			command.GetIncrement().Value++
+			log.Println("Increment Command")
+		default:
 		}
 		log.Printf("recv: %s", command)
 		out, err := proto.Marshal(command)
@@ -57,4 +62,3 @@ func main() {
 	http.HandleFunc("/", echo)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
-
